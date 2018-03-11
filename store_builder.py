9fad7f3788c4cdc364c89b269cb5c37e89d90b25
DasFranck Hochstaetter <dasfranck@gmx.com>
2018-03-11 15:42:56 +0100

    I guess it's finished for part 1

--- store_builder.py
+++ store_builder.py
@@ -8,6 +8,7 @@ def store_triples(tsv_content):
     DBO = Namespace('http://dbpedia.org/ontology/')
     GEO = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
     SCHEMA = Namespace('http://schema.org/')
+    NGW = Namespace('local:ngwproject:')
 
     store = Graph()
     store.bind("owl", OWL)
@@ -16,37 +17,37 @@ def store_triples(tsv_content):
     store.bind("dbo", DBO)
     store.bind("geo", GEO)
     store.bind("schema", SCHEMA)
+    store.bind("ngw", NGW)
 
-    hospital_list = []
     for hospital in tsv_content:
         node = BNode()
         store.add((node, RDF.type, SCHEMA.Organisation))
-        # Organisation ID
-        # Organisation Code
-        # Organisation Type
-        # Organisation SubType
-        # Organisation Sector
+        store.add((node, NGW["organisationID"], Literal(hospital[0])))
+        store.add((node, NGW["organisationCode"], Literal(hospital[1])))
+        store.add((node, NGW["organisationType"], Literal(hospital[2])))
+        store.add((node, NGW["organisationSubType"], Literal(hospital[3])))
+        store.add((node, NGW["organisationSector"], Literal(hospital[4])))
         store.add((node, DBO.status, Literal(hospital[5])))
-        # IsPimsManaged
+        store.add((node, NGW["isPimsManaged"], Literal(hospital[6])))
         store.add((node, DBO.name, Literal(hospital[7])))
-        # Address1
-        # Address2
-        # Address3
+        store.add((node, NGW["address1"], Literal(hospital[8])))
+        store.add((node, NGW["address2"], Literal(hospital[9])))
+        store.add((node, NGW["address3"], Literal(hospital[10])))
         store.add((node, DBO.city, Literal(hospital[11])))
         store.add((node, DBO.county, Literal(hospital[12])))
         store.add((node, DBO.postalCode, Literal(hospital[13])))
         store.add((node, GEO.latitude, Literal(hospital[14])))
         store.add((node, GEO.longitude, Literal(hospital[15])))
-        # ParentODSCode
-        # ParentName
+        store.add((node, NGW["parentODSCode"], Literal(hospital[16])))
+        store.add((node, NGW["parentName"], Literal(hospital[17])))
         store.add((node, FOAF.phone, Literal(hospital[18])))
         store.add((node, FOAF.email, Literal(hospital[19])))
         store.add((node, FOAF.homepage, Literal(hospital[20])))
         store.add((node, SCHEMA.faxNumber, Literal(hospital[21])))
 
-    #print("--- printing raw triples ---")
-    #for s, p, o in store:
-    #    print(s, p, o)
+    print("--- printing raw triples ---")
+    for s, p, o in store:
+        print(s, p, o)
 
     with open("results.ttl", mode='wb') as fd:
         fd.write(store.serialize(format="turtle", encoding="utf-8"))
